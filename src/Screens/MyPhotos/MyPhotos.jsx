26f40213867e38447b19bab645f3125030728596
asiasumo <asiasumo@gmail.com>
2020-04-26 17:37:58 +0200

    my galerry photos render

--- src/Screens/MyPhotos/MyPhotos.jsx
+++ src/Screens/MyPhotos/MyPhotos.jsx
@@ -1,13 +1,41 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./MyPhotosStyle.css";
 import NavigationBar from "../../Components/NavigationBar";
 import { Link } from "react-router-dom";
 import UserAuthenticationHandler from "../../Handlers/UserAuthenticationHandler";
+import * as firebase from "firebase/app";
+import "firebase/firestore"
+import 'firebase/storage';
+import ImageCardUploaded from "./ImageCardUploaded";
+import { CardDeck } from "react-bootstrap";
+
 
 export default function MyPhotos() {
   const [user, setUser] = useState({})
-  UserAuthenticationHandler.addListener(user=>setUser(user))
-  if(!user)UserAuthenticationHandler.redirectToHome()
+  UserAuthenticationHandler.addListener(user => setUser(user))
+  if (!user) UserAuthenticationHandler.redirectToHome()
+
+
+  const [urls, setUrls] = useState([])
+  const elem = []
+  const db = firebase.firestore()
+  const storage = firebase.storage();
+
+
+  const downloadPhotos = () => {
+    storage.ref(`${user.uid}/`).listAll()
+      .then(function (result) {
+        result.items.forEach(function (imageRef) {
+          elem.push(<ImageCardUploaded value={imageRef} />)
+        })
+        setUrls(elem);
+
+      });
+  }
+  if (urls.length == 0) {
+    downloadPhotos();
+  }
+
   return (
     <div>
       <NavigationBar />
@@ -18,8 +46,9 @@ export default function MyPhotos() {
             Click me :)
           </Link>
         </h1>
-        {/* or */}
-        {/* <ImageCardUploaded /> */}
+        <CardDeck className="imageCardDeck">
+          {urls}
+        </CardDeck>
       </div>
     </div>
   );
---
+++ src/Screens/MyPhotos/MyPhotosWrapper.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+export default function RenderWrapper(props) {
+
+    return (
+        <div>
+            {props.elem}
+        </div>
+    )
+}
