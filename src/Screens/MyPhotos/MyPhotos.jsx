14dd1339fb440e9443b1d20f93ea8120d522f09c
jacekku <kecajwy@gmail.com>
2020-04-27 21:45:15 +0200

    fixing myPhotos

--- src/Screens/MyPhotos/MyPhotos.jsx
+++ src/Screens/MyPhotos/MyPhotos.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./MyPhotosStyle.css";
 import NavigationBar from "../../Components/NavigationBar";
 import { Link } from "react-router-dom";
@@ -10,42 +10,46 @@ import ImageCardUploaded from "./ImageCardUploaded";
 import { CardDeck } from "react-bootstrap";
 
 export default function MyPhotos() {
-  const [user, setUser] = useState({});
-  UserAuthenticationHandler.addListener((user) => setUser(user));
-  if (!user) UserAuthenticationHandler.redirectToHome();
+	const [user, setUser] = useState({});
+	UserAuthenticationHandler.addListener((user) => setUser(user));
+	if (!user) UserAuthenticationHandler.redirectToHome();
 
-  const [urls, setUrls] = useState([]);
-  const elem = [];
-  const db = firebase.firestore();
-  const storage = firebase.storage();
+	const [urls, setUrls] = useState([]);
+	const elem = [];
+	const storage = firebase.storage();
+	const downloadPhotos = () => {
+		storage
+			.ref(`${user.uid}/`)
+			.listAll()
+			.then(function (result) {
+				result.items.forEach(function (imageRef) {
+					elem.push(<ImageCardUploaded value={imageRef} user={user} />);
+				});
+				setUrls(elem);
+			});
+	};
 
-  const downloadPhotos = () => {
-    storage
-      .ref(`${user.uid}/`)
-      .listAll()
-      .then(function (result) {
-        result.items.forEach(function (imageRef) {
-          elem.push(<ImageCardUploaded value={imageRef} />);
-        });
-        setUrls(elem);
-      });
-  };
-  if (urls.length == 0) {
-    downloadPhotos();
-  }
+	useEffect(() => {
+		if (!urls.length) {
+			downloadPhotos();
+		}
+	});
 
-  return (
-    <div>
-      <NavigationBar />
-      <div>
-        <h1 className="info">No images yet? Upload your first picture!</h1>
-        <h1 className="info2">
-          <Link className="info2" to="post">
-            Click me :)
-          </Link>
-        </h1>
-        <CardDeck className="imageCardDeck">{urls}</CardDeck>
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<NavigationBar />
+			{!urls.length ? (
+				<div>
+					<h1 className="info">No images yet? Upload your first picture!</h1>
+					<h1 className="info2">
+						<Link className="info2" to="post">
+							Click me :)
+						</Link>
+					</h1>
+				</div>
+			) : (
+				<CardDeck className="imageCardDeck">{urls}</CardDeck>
+			)}
+		</div>
+	);
 }
